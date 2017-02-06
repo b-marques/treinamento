c5b2b7fb9a856da4625eb295fb2776b64abc0a59
Bruno Marques <brunomn95@gmail.co>
2017-02-06 15:46:20 -0200

    Everything working

--- src/main/java/bruno/ExercicioSelenium.java
+++ src/main/java/bruno/ExercicioSelenium.java
@@ -20,19 +20,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import main.BuscaConfig;
 import main.Delay;
 import main.Exercicio;
+import main.IntervaloDeBusca;
 
 public class ExercicioSelenium implements Exercicio {
 
 	public static void main(String[] args) {
 		ExercicioSelenium ex = new ExercicioSelenium();
 		
-//		long result = ex.getNumeroAproximadoDoResultadoDaBuscaPor("metallica");
-//		System.out.println(result);
+		String termo = "Metallica";
+		int pagina = 2;
+		IntervaloDeBusca intervalo = IntervaloDeBusca.NO_ULTIMO_ANO;
 		
+		BuscaConfig config = new BuscaConfig();
+		config.setPagina(pagina);
+		config.setTermo(termo);
+		config.setIntervalo(intervalo);
 		
-		List<String> result = ex.getUrls("metallica", 15);
-		for (String link : result)
-			System.out.println(link);
+		List<String> result = ex.getUrls(config);
+		for(String url: result)
+			System.out.println(url);
 
 	}
 	
@@ -142,13 +148,123 @@ public class ExercicioSelenium implements Exercicio {
 	}
 	
 	public List<String> getUrls(BuscaConfig config) {
-		// TODO Auto-generated method stub
-		return null;
+		WebDriver webdriver = null;
+		List<String> result = new ArrayList<String>();
+		
+		try {
+			webdriver = startWebDriver();
+		
+			webdriver.get("https://www.google.com.br/");
+			WebDriverWait wait = new WebDriverWait(webdriver, 10);
+			
+			WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("lst-ib")));
+			element.sendKeys(config.getTermo());
+			wait.until(ExpectedConditions.elementToBeClickable(By.id("_fZl"))).click();
+			
+			switch (config.getIntervalo()) {
+			case EM_QUALQUER_DATA:	
+				break;
+			
+			case NA_ULTIMA_HORA:
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("hdtb-tls"))).click();
+				wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("resultStats")));
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='mn-hd-txt' and contains(text(),'Em qualquer data')]"))).click();
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='q qs' and contains(text(),'Na última hora')]"))).click();
+				break;
+			
+			case NAS_ULTIMAS_24_HORAS:
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("hdtb-tls"))).click();
+				wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("resultStats")));
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='mn-hd-txt' and contains(text(),'Em qualquer data')]"))).click();
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='q qs' and contains(text(),'Nas últimas 24 horas')]"))).click();
+				break;
+			
+			case NA_ULTIMA_SEMANA:
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("hdtb-tls"))).click();
+				wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("resultStats")));
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='mn-hd-txt' and contains(text(),'Em qualquer data')]"))).click();
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='q qs' and contains(text(),'Na última semana')]"))).click();
+				break;
+			
+			case NO_ULTIMO_MES:
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("hdtb-tls"))).click();
+				wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("resultStats")));
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='mn-hd-txt' and contains(text(),'Em qualquer data')]"))).click();
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='q qs' and contains(text(),'No último mês')]"))).click();
+				break;
+			
+			case NO_ULTIMO_ANO:
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("hdtb-tls"))).click();
+				wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("resultStats")));
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='mn-hd-txt' and contains(text(),'Em qualquer data')]"))).click();
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='q qs' and contains(text(),'No último ano')]"))).click();
+				break;
+			
+			case INTERVALO_PERSONALIZADO:
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("hdtb-tls"))).click();
+				wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("resultStats")));
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='mn-hd-txt' and contains(text(),'Em qualquer data')]"))).click();
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("cdrlnk"))).click();
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("cdr_min"))).sendKeys("01/01/2017");
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("cdr_max"))).sendKeys("02/02/2017");
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//form[@id='cdr_frm']/input[@type='submit']"))).click();
+				break;
+				
+			default:
+				break;
+			}
+			
+			if (config.getPagina() < 11) {
+				if (config.getPagina() != 1)
+					wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("flyr")));
+					wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td/a[@aria-label='Page "+config.getPagina()+"']"))).click();
+			} else {
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td/a[@aria-label='Page 10']"))).click();
+				
+				for (int i = 0; i < config.getPagina()-10; i++) {
+					wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("flyr")));
+					wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@id='pnnext']"))).click();
+				}
+			}
+			
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//td[@class='cur' and contains(text(),'"+config.getPagina()+"')]")));
+			List<WebElement> resultSet = webdriver.findElements(By.xpath("//h3[@class='r']/a"));
+			
+			for(WebElement link: resultSet)
+				result.add(link.getAttribute("href"));
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		} finally {
+			closeWebDriver(webdriver);
+		}
+		return result;
 	}
 	
 	public String getWikiResume(String termo) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		WebDriver webdriver = null;
+		String result = null;
+		
+		try {
+			webdriver = startWebDriver();
+		
+			webdriver.get("https://www.google.com.br/");
+			WebDriverWait wait = new WebDriverWait(webdriver, 10);
+			
+			WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("lst-ib")));
+			element.sendKeys(termo);
+			
+			WebElement element1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@jsl='$t t-JgTEvN6zUII;$x 0;']/span")));
+			
+			result = element1.getText();
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		} finally {
+			closeWebDriver(webdriver);
+		}
+		return result;
 	}
 	
 	private WebDriver startWebDriver(){
